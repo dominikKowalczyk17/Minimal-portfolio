a91c6d273a083fd26fcf50bedb9ec289b978e775
Dominik Kowalczyk <dominik.kowalczyk17@gmail.com>
2024-02-11 20:47:43 +0100

    test2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dominikKowalczyk17.github.io/",
+  base: "/Minimal-portfolio/",
   plugins: [react()],
 });
