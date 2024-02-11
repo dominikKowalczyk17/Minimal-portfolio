8cf02d623b90773e7960b31b269419415039a7b3
Dominik Kowalczyk <dominik.kowalczyk17@gmail.com>
2024-02-11 20:56:59 +0100

    path change

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,13 +1,13 @@
 export default [
   {
     title: "Coffe Shop",
-    imgUrl: "/assets/coffee_shop.png",
+    imgUrl: "assets/coffee_shop.png",
     stack: ["React", "SCSS", "HTML"],
     link: "https://github.com/dominikKowalczyk17/Coffe-Shop",
   },
   {
     title: "Jet Story",
-    imgUrl: "/assets/JetStory.png",
+    imgUrl: "assets/JetStory.png",
     stack: ["JavaScript", "SCSS", "Webpack"],
     link: "https://jetstory.com/",
   },
