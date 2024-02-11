14cbc013511712147c5bafe8af4539f48ceedb81
Dominik Kowalczyk <dominik.kowalczyk17@gmail.com>
2024-02-11 20:50:47 +0100

    adding dist

---
+++ dist/assets/timeline.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    year: "Today",
+    title: "Junior Frontend Developer",
+    duration: "6 months",
+    details:
+      "I'm entrusted with the care of existing websites within the Polsat Media group, ensuring their smooth operation. Additionally, I'm actively involved in crafting new websites for our expanding clientele as well as developing new features for existing websites. In this dynamic role, I leverage technologies such as JavaScript, SCSS, ES6, and Velocity to bring innovative digital experiences to life.",
+  },
+  {
+    year: "2021",
+    title: "Began learning Frontend development",
+    duration: "2 years",
+    details:
+      "I've decided to try something new that I've been wanting to attempt for a very long time, so I began learning how to code. It took me 2 years, with some breaks, to land my first job.",
+  },
+  {
+    year: "2020",
+    title: "Bachelor's Degree in Electrical Engineering",
+    duration: "4 years",
+    details:
+      "I've successfully concluded my studies in Electrical Engineering at Lodz University of Technology, although I didn't proceed to defend my thesis or attain a formal title.",
+  },
+];
