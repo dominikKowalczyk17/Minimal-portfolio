7978a73a0b6150e9da84e217f53f881b96a0399f
dkowalczyk <d.kowalczyk@dituel.pl>
2024-05-24 10:57:58 +0200

    add new website, update timeline

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,26 @@
 export default [
   {
-    title: "Coffe Shop",
-    imgUrl: "assets/coffee_shop.png",
-    stack: ["React", "SCSS", "HTML"],
-    link: "https://github.com/dominikKowalczyk17/Coffe-Shop",
+    title: 'Coffe Shop',
+    imgUrl: 'assets/coffee_shop.png',
+    stack: ['React', 'SCSS', 'HTML'],
+    link: 'https://github.com/dominikKowalczyk17/Coffe-Shop',
   },
   {
-    title: "Jet Story",
-    imgUrl: "assets/JetStory.png",
-    stack: ["JavaScript", "SCSS", "Webpack"],
-    link: "https://jetstory.com/",
+    title: 'Jet Story',
+    imgUrl: 'assets/JetStory.png',
+    stack: ['JavaScript', 'SCSS', 'Webpack'],
+    link: 'https://jetstory.com/',
   },
   {
-    title: "Nike Store",
-    imgUrl: "assets/NikeStore.png",
-    stack: ["React", "Tailwind", "Webpack"],
-    link: "https://dominikkowalczyk17.github.io/Nike-Store/",
+    title: 'Nike Store',
+    imgUrl: 'assets/NikeStore.png',
+    stack: ['React', 'Tailwind', 'Webpack'],
+    link: 'https://dominikkowalczyk17.github.io/Nike-Store/',
   },
-];
+  {
+    title: 'Neso Stacje',
+    imgUrl: 'assets/neso_station.jpg',
+    stack: ['Webpack', 'JavaScript', 'SCSS'],
+    link: 'https://stacjeneso.pl/',
+  },
+]
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,23 +1,30 @@
 export default [
   {
-    year: "06.2023",
-    title: "Junior Frontend Developer",
-    duration: "Today",
+    year: '2024',
+    title: 'Junior Frontend Developer',
+    duration: 'Today',
+    details:
+      'Co-Developed New Polsat Media Website:\n- Requirement Analysis: Collaborated with the client to understand and document specifications.\n- Project Coordination: Facilitated communication between client, designers, and developers.\n- Design Integration: Worked with graphic designers to merge aesthetics with functionality.\n- Technical Development: Utilized modern web technologies for a responsive and high-performance site.\n- Quality Assurance: Conducted thorough testing to ensure a bug-free launch.\n- Client Feedback: Integrated client feedback to continuously improve the website.\n- Deployment and Support: Managed deployment and provided post-launch support.',
+  },
+  {
+    year: '2023',
+    title: 'Junior Frontend Developer',
+    duration: 'Present',
     details:
       "I'm entrusted with the care of existing websites within the Polsat Media group, ensuring their smooth operation. Additionally, I'm actively involved in crafting new websites for our expanding clientele as well as developing new features for existing websites. In this dynamic role, I leverage technologies such as JavaScript, SCSS, ES6, and Velocity to bring innovative digital experiences to life.",
   },
   {
-    year: "2021",
-    title: "Began learning Frontend development",
-    duration: "2 years",
+    year: '2021',
+    title: 'Began learning Frontend development',
+    duration: '2 years',
     details:
       "I've decided to try something new that I've been wanting to attempt for a very long time, so I began learning how to code. It took me 2 years, with some breaks, to land my first job.",
   },
   {
-    year: "2020",
+    year: '2020',
     title: "Bachelor's Degree in Electrical Engineering",
-    duration: "4 years",
+    duration: '4 years',
     details:
       "I've successfully concluded my studies in Electrical Engineering at Lodz University of Technology, although I didn't proceed to defend my thesis or attain a formal title.",
   },
-];
+]
